5a496b958de9f141dc7a3ffbf922a2982aa9504c
Dave Wheeler <dwheelerau@gmail.com>
2018-04-06 14:41:16 +1000

    add mouse

--- tmux-dpi-session.sh
+++ tmux-dpi-session.sh
@@ -3,41 +3,41 @@
 SESSION=$USER
 
 # start a new tmux session
-tmux -2 new-session -d -s $SESSION
+tmux -2 new-session -d -s $SESSION -n 'win1'
 
 # session one normal bash window on local PC
 # with verticle split
-tmux new-window -t $SESSION:0 -n 'LocalPC'
+tmux new-window -t $SESSION:0 -n 'win2'
 
 # ssh to ngbs
-# tmux new-window -t $SESSION:1 -n 'NgBS-server'
-# tmux send-keys "sshngbs" C-m
-# tmux split-window -v
-# tmux select-pane -t 1
-# tmux resize-pane -D 17
-# tmux select-pane -t 0
+#tmux new-window -t $SESSION:1 -n 'NgBS-server'
+#tmux send-keys "sshngbs" C-m
+#tmux split-window -v
+#tmux select-pane -t 1
+#tmux resize-pane -D 17
+#tmux select-pane -t 0
 
 # ssh to nzgl
-# tmux new-window -t $SESSION:2 -n 'NZGL-server'
-# tmux send-keys "sshnzgl" C-m
-# tmux split-window -v
-# tmux select-pane -t 1
-# tmux resize-pane -D 17
-# tmux select-pane -t 0
-
-
-#tmux split-window -h
+#tmux new-window -t $SESSION:2 -n 'NZGL-server'
+#tmux send-keys "sshnzgl" C-m
+#tmux split-window -v
+#tmux select-pane -t 1
+#tmux resize-pane -D 17
 #tmux select-pane -t 0
+
+tmux split-window -h
+tmux select-pane -t 0
 # use -f to keep this live ie streaming content
 #tmux send-keys "tail /var/mail/dwheeler" C-m
 # now the next pane
-#tmux select-pane -t 1
-#tmux send-keys "echo 'hello1'" C-m
-#tmux split-window -v
-#tmux resize-pane -D 20
-#tmux send-keys "echo 'hello2'" C-m
+# -t 0 is left, -t 1 is right
+tmux select-pane -t 0
+tmux send-keys "htop" C-m
+tmux split-window -v
+tmux resize-pane -D 0
 # Set default window
-tmux select-window -t $SESSION:0
+tmux select-window -t $SESSION:1
+#tmux select-pane -t 1
 
 # Attach to session and change to current directory
 tmux -2 attach-session -t $SESSION -c $PWD
